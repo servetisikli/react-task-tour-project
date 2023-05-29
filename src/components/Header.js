690f4253a135f8f3e0774197ef8a3b9f866430c3
servetisikli <servetisikli@gmail.com>
2023-05-29 12:34:17 +0200

    header.js edited

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title = "Task Tracker" }) => {
+  const handleClick = () => {
+    console.log("Click with handleClick");
+  };
+
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="red" text="Show Add Task Bar" handleClick={handleClick} />
     </header>
   );
 };
